0b1bdd3afc993cd5452fb2aacef9d83bfa20b332
Thomas C. Furfaro <tfurfaro@gmail.com>
2014-11-12 16:00:38 +0100

    Updated, try this.git add ExTkinter.py !

--- ExTkinter.py
+++ ExTkinter.py
@@ -1,15 +1,19 @@
 from Tkinter import *
 
 class Program:
+    def __init__(self):
+        self.r = Tk()           # We have to initialize it first.
+        self.gr1 = IntVar()     # And initialize the place where we'll have the info.
+        a = Radiobutton(text="Option A", command=self.callback, variable = self.gr1, value = 1).pack()
+        b = Radiobutton(text="Option B", command=self.callback, variable = self.gr1, value = 2).pack()
+        c = Radiobutton(text="Option C", command=self.callback, variable = self.gr1, value = 3).pack()
 
-def __init__(self):
-    a = Radiobutton(text="Option A", command=self.callback, variable = "gr1", value = 1).pack()
-    b = Radiobutton(text="Option B", command=self.callback, variable = "gr1", value = 2).pack()
-    c = Radiobutton(text="Option C", command=self.callback, variable = "gr1", value = 3).pack()
 
+    def callback(self):
+        print "clicked!"
+        print "The state of gr1 is " + str(self.gr1.get())
 
-def callback(self):
-    print "clicked!"
-program = Program()
+if __name__ == '__main__':
+    program = Program()
 
-mainloop()
+    mainloop()
